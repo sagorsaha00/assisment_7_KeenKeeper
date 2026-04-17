ea4ef8eafe7009a9ed68fac43cbbbaad50f89103
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-04-17 10:55:20 +0600

    header modify

--- src/allComponent/header.jsx
+++ src/allComponent/header.jsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import { IoHomeOutline } from "react-icons/io5";
 import { MdOutlineWatchLater } from "react-icons/md";
 import { FaChartLine } from "react-icons/fa";
-
+import { useNavigate } from "react-router-dom";
 export default function Header() {
   const DisplayWidth = window.innerWidth < 768;
   const [MobileScreen, setMobileScreen] = useState(DisplayWidth);
   const [mobilemenu, setMobilemenu] = useState(false);
+  const navigate = useNavigate();
 
   function handleShow() {
     setMobilemenu(!mobilemenu);
@@ -25,27 +26,32 @@ export default function Header() {
   return (
     <header className="w-full bg-white shadow">
       <div className="w-[95%] md:w-[90%] mx-auto h-20 flex items-center justify-between">
-       <div>
-          <img
-            src="/logo.png"
-            alt="logo"
-            className="h-10 md:h-12 object-contain"
-          />
-        </div>
+        <img
+          onClick={() => navigate("/")}
+          src="/logo.png"
+          alt="logo"
+          className="h-10 md:h-12 object-contain cursor-pointer"
+        />
 
         {!MobileScreen && (
           <nav className="flex items-center gap-8 text-lg">
-            <button className="cursor-pointer flex items-center gap-2 bg-[#064E3B] text-white px-5 py-2 rounded-md hover:bg-green-800 transition">
+            <button
+              onClick={() => navigate("/")}
+              className="cursor-pointer flex items-center gap-2 bg-[#064E3B] text-white px-5 py-2 rounded-md hover:bg-green-800 transition"
+            >
               <IoHomeOutline />
               <span className="font-medium">Home</span>
             </button>
 
-            <button className="cursor-pointer flex items-center gap-2 text-gray-500 hover:text-gray-800 transition">
+            <button
+              onClick={() => navigate("/timeline")}
+              className="cursor-pointer flex items-center gap-2 text-gray-500 hover:text-gray-800 transition"
+            >
               <MdOutlineWatchLater />
               <span>Timeline</span>
             </button>
 
-            <button className="cursor-pointer flex items-center gap-2 text-gray-500 hover:text-gray-800 transition">
+            <button onClick={() => navigate('/paichart')} className="cursor-pointer flex items-center gap-2 text-gray-500 hover:text-gray-800 transition">
               <FaChartLine />
               <span>Stats</span>
             </button>
