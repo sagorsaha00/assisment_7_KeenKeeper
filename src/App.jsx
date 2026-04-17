2e308cfd4c451d172631ca4f8d7d4a7f7deb175b
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-04-17 10:54:20 +0600

    context api add and error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Header from "./allComponent/header";
-import HeroSection from "./allComponent/textSection";
-export default function App() {
+import { useState } from "react";
+import { RouterProvider } from "react-router-dom";
+import { ThemeContext } from "./apiContext.js";
+import router from "./router/route.jsx";
+import { ToastContainer } from "react-toastify";
+
+export default function Root() {
+  const [timeline, setTimeline] = useState([]);
+  const [user, setUser] = useState([]);
   return (
-    <>
-      <Header />
-      <HeroSection />
-    </>
+    <ThemeContext.Provider value={{ timeline, setTimeline, user, setUser }}>
+      <RouterProvider router={router} />
+      <ToastContainer theme="dark" position="top-center" />
+    </ThemeContext.Provider>
   );
 }
